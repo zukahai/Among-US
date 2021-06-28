a664decf512b77ecb861b9273ae20958a25ffa9b
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-06-29 03:34:38 +0700

    a

--- js/game.js
+++ js/game.js
@@ -47,11 +47,10 @@ class game {
 
     listenTouch() {
         document.addEventListener("touchmove", evt => {
-            if (evt) evt = window.event;
             console.log(evt.touches.length);
-            this.amu.name = evt.touches.length;
-            var x = evt.touches[0].pageX;
-            var y = evt.touches[0].pageY;
+            this.amu.name = "Hello";
+            var x = evt.touches[evt.touches.length - 1].pageX;
+            var y = evt.touches[evt.touches.length - 1].pageY;
 
             var Xc = this.getWidth() * 3.5;
             var Yc = game_H - this.getWidth() * 3.5;
