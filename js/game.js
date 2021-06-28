aab53e3ed760ebc20cf106e9fbde2fe708a4bf6e
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-06-28 18:06:12 +0700

    HaiZuka

--- js/game.js
+++ js/game.js
@@ -184,7 +184,10 @@ class game {
 
     clearScreen() {
         this.context.clearRect(0, 0, game_W, game_H);
-        this.context.drawImage(bg, 0, 0, game_W, game_H);
+        if (bg.width / game_W < bg.height / game_H)
+            this.context.drawImage(bg, 0, (bg.height - game_H * (bg.width / game_W)) / 2, bg.width, game_H * (bg.width / game_W), 0, 0, game_W, game_H);
+        else
+        this.context.drawImage(bg, (bg.width - game_W * (bg.height / game_H)) / 2, 0, game_W * (bg.height / game_H), bg.height, 0, 0, game_W, game_H);
         // this.context.fillStyle = "#000000";
         // this.context.fillRect(0, 0, game_W, game_H);
     }
